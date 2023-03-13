db84b8cc5f18cddfe887bb10716219e3004abaaa
Ogunbiyi Elijah <115193430+Akins20@users.noreply.github.com>
2023-03-13 21:35:45 +0100

    Update Asgn5_L2.js

--- 30days of Javascripting/Week-Five/Assignment5_L2/Asgn5_L2.js
+++ 30days of Javascripting/Week-Five/Assignment5_L2/Asgn5_L2.js
@@ -1,7 +1,7 @@
 //ExERCISE2
 //question1: Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
-import { countries } from "./countries.js";
-import { webTechs } from "./web_techs.js";
+import { countries } from "./countries";
+import { webTechs } from "./web_techs";
 console.log(countries, webTechs);
 
 //question2: First remove all the punctuations and change the string to array and count the number of words in the array
